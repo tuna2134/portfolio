693832dd34f15b640c2293571f19b1e1c5b12615
tuna2134 <me@tuna2134.dev>
2023-10-01 17:05:58 +0000

    add header

--- app/header.tsx
+++ app/header.tsx
@@ -1,1 +1,30 @@
 import Link from "next/link";
+import React from "react";
+import styles from "./header.module.scss";
+
+export const Header: React.FC = () => {
+    const linkItems = [
+        {
+            name: "language",
+            value: "/#language",
+        },
+        {
+            name: "discord",
+            value: "/#discord",
+        }
+    ]
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerHW}>
+                <div className={styles.title}>
+                    <p>tn23</p>
+                </div>
+                <nav className={styles.navItems}>
+                    {linkItems.map((linkItem, index) => (
+                        <Link className={styles.navItem} href={linkItem.value} key={index}>{linkItem.name}</Link>
+                    ))}
+                </nav>
+            </div>
+        </header>
+    )
+};
