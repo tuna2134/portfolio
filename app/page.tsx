4f3cb384f64787b2355891110315c9e8a4652aab
tuna2134 <me@tuna2134.dev>
2023-10-03 11:44:07 +0000

    add gradation

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,14 @@ export default async function Home() {
     },
   });
   const user = await res.json();
+  const IMAGE_URL = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?height=${IMAGE_SIZE}&width=${IMAGE_SIZE}`
   return (
     <main>
       <div className={styles.main__one}>
         <div className={styles.main__one_children}>
           <div className={styles.main__one_children_content}>
             <div className={styles.main__one_children_img}>
-              <img width={IMAGE_SIZE} height={IMAGE_SIZE} src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}></img>
+              <img width={IMAGE_SIZE} height={IMAGE_SIZE} src={IMAGE_URL}></img>
             </div>
             <div>
               <h1>tuna2134</h1>
